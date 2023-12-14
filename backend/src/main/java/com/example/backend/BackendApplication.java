a0ff6aab882c3e7b0aa12238c31e1647f2134c91
“Your <you@example.com>
2023-12-14 09:14:31 +0530

    formatted the code

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class BackendApplication {
 
 	private static final Logger logger = LogManager.getLogger(BackendApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		logger.info("SpringBoot Application Live");
